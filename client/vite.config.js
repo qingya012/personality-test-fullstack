e01061209eb9a89193d925aaa6090af8260052fc
Qingya Zhang <zhang_qingya012@outlook.com>
2026-02-01 12:45:55 -0800

    update homepage and remote

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  base: '/scent-personality-quiz/',
+  base: '/personality-test-fullstack/',
   plugins: [react(), tailwindcss()],
   build: {
     rollupOptions: {
